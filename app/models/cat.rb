ea3d9dcad343e0a5c6eeb51d330371074a61b80e
Oscar & Judy <oscar-judy@aa.io>
2015-08-18 13:54:53 -0400

    Add #new and #show actions and views.

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,7 +1,12 @@
 class Cat < ActiveRecord::Base
-  ALLOWED_COLORS = [:orange, :black, :white, :gray, :brown]
+  ALLOWED_COLORS = ["orange", "black", "white", "gray", "brown"]
   validates :color, presence: true, inclusion: { in: ALLOWED_COLORS, message: "color not allowed" }
   validates :name, presence: true
   validates :sex, presence: true, inclusion: { in: ["M", "F"], message: "invalid gender"}
   validates :birth_date, presence: true
+
+  def age
+     (Time.now.to_date - birth_date).to_i / 365
+  end
+
 end
